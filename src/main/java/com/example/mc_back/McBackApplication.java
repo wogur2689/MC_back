1d09f9dac4f4e07544d0fb164de5ce04d5607695
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-10-20 23:47:48 +0900

    Initial commit

---
+++ src/main/java/com/example/mc_back/McBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mc_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mc_back/McBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mc_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
