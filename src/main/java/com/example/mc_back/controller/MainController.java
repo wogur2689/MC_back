d18724f01648d91b808730e001c8e5a1f8960c70
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-12-21 00:06:36 +0900

    [feat]리턴 코드,메세지 추가

--- src/main/java/com/example/mc_back/controller/MainController.java
+++ src/main/java/com/example/mc_back/controller/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -37,7 +38,7 @@ public class MainController {
         }
         log.info("### 요청값 : {}", supportUser);
 
-        Map<String, Object> map = null;
+        Map<String, Object> map = new HashMap<>();
         map.put("name", supportUser.getName());
         map.put("email", supportUser.getEmail());
         map.put("phone", supportUser.getPhone());
--- src/main/java/com/example/mc_back/param/SupportUser.java
+++ src/main/java/com/example/mc_back/param/SupportUser.java
@@ -1,10 +1,16 @@
 package com.example.mc_back.param;
 
+import com.example.mc_back.enums.ApiCode;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class SupportUser {
+    @NotNull(message = "이름을 입력해주세요.")
     private String name;    //이름
+    @NotNull(message = "이메일을 입력해주세요.")
     private String email;   //이메일
+    @NotNull(message = "전화번호를 입력해주세요.")
     private String phone;   //핸드폰번호
 }
